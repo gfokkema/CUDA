b24e56fcfb7314fa34c56eca0082e99a2951c404
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2014-07-09 16:10:49 +0200

    Select OpenCL device with the highest version number

--- src/window.cpp
+++ src/window.cpp
@@ -1,6 +1,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "CL/cl.hpp"
+
 #include "util/camera.h"
 #include "scene.h"
 
@@ -11,6 +13,7 @@ namespace {
 	GLFWwindow* window;
 	Camera camera(WIDTH, HEIGHT);
 	Scene scene;
+	cl::Device device;
 
 	/**
 	* Time independent keyboard function
@@ -66,6 +69,42 @@ namespace {
 		handle_keyboard(dt);
 		handle_mouse();
 	}
+
+	int initOpenCL(cl::Device& device) {
+		cl_int err;
+		std::vector<cl::Platform> platforms;
+
+		err = cl::Platform::get(&platforms);
+		if (err != CL_SUCCESS) return err;
+
+		float max_ver;
+		for (int i = 0; i < platforms.size(); i++) {
+			std::vector<cl::Device> devices;
+			err = platforms[i].getDevices(CL_DEVICE_TYPE_ALL, &devices);
+			if (err != CL_SUCCESS) continue;
+
+			std::string version;
+			err = platforms[i].getInfo(CL_PLATFORM_VERSION, &version);
+			if (err != CL_SUCCESS) continue;
+
+			float ver = atof(version.substr(7,3).c_str());
+			if (ver > max_ver && devices.size() > 0) {
+				max_ver = ver;
+				device = devices[0];
+			}
+		}
+
+		std::string platversion, devicename;
+		err = device.getInfo(CL_DEVICE_VERSION, &platversion);
+		if (err != CL_SUCCESS) return err;
+		err = device.getInfo(CL_DEVICE_NAME, &devicename);
+		if (err != CL_SUCCESS) return err;
+
+		std::cout << "Selected platform:\t" << platversion << std::endl;
+		std::cout << "Selected device:\t" << devicename << std::endl;
+
+		return CL_SUCCESS;
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -91,6 +130,9 @@ int main(int argc, char* argv[]) {
 	}
 
 	scene.setCamera(&camera);
+
+	// Initalize OpenCL device
+	initOpenCL(device);
 
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
