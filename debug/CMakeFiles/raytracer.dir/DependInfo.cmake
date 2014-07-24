b5a3c0b0eb49c4ae87f4ba9f60fd311c06894ceb
Balletie <skip_meesie@hotmail.com>
2014-07-24 14:10:30 +0200

    CMakeLists.txt in kernel dir now generates LLVM bitcode files, and generated header and source file for OSX support.
    Some extra changes: split up definition of shape struct (otherwise the files wouldn't be correctly generated)
    Allocate memory for the camera struct and pass it as a pointer (some OpenCL implementations have problems passing structs 'directly')

---
+++ debug/CMakeFiles/raytracer.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/skip/Programming/C++/RT-Raytracing/src/scene.cpp" "/Users/skip/Programming/C++/RT-Raytracing/debug/CMakeFiles/raytracer.dir/scene.cpp.o"
+  "/Users/skip/Programming/C++/RT-Raytracing/src/session.cpp" "/Users/skip/Programming/C++/RT-Raytracing/debug/CMakeFiles/raytracer.dir/session.cpp.o"
+  "/Users/skip/Programming/C++/RT-Raytracing/src/window.cpp" "/Users/skip/Programming/C++/RT-Raytracing/debug/CMakeFiles/raytracer.dir/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/skip/Programming/C++/RT-Raytracing/debug/devices/CMakeFiles/devices.dir/DependInfo.cmake"
+  "/Users/skip/Programming/C++/RT-Raytracing/debug/util/CMakeFiles/util.dir/DependInfo.cmake"
+  "/Users/skip/Programming/C++/RT-Raytracing/debug/kernel/CMakeFiles/kernel.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/cuda/include"
+  "/opt/X11/include"
+  "/opt/local/include"
+  "/Users/skip/Programming/C++/RT-Raytracing/src"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
